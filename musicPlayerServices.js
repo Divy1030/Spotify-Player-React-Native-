dc1799e8b40d62a371f485549012045d76419236
Divy1030 <157683952+Divy1030@users.noreply.github.com>
2024-12-29 20:51:36 +0530

    Implement player setup and event handling for music playback

--- musicPlayerServices.js
+++ musicPlayerServices.js
@@ -1,7 +1,39 @@
-import TrackPlayer from "react-native-track-player";
-
 import playListData from "./src/constants";
+import TrackPlayer, { RepeatMode } from 'react-native-track-player';
+
+export async function setupPlayer() {
+    let isSetup = false;
+    try {
+        const currentTrack = await TrackPlayer.getCurrentTrack();
+        if (currentTrack !== null) {
+            isSetup = true;
+        } else {
+            throw new Error('No current track');
+        }
+    } catch (e) {
+        await TrackPlayer.setupPlayer();
+        isSetup = true;
+    } finally {
+        return isSetup;
+    }
+}
+
+export async function addReack(){
+    await TrackPlayer.add(playListData);
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
 
 export async function playBackService(){
-    
+    TrackPlayer.addEventListener(Event.RemotePause,()=>{
+        TrackPlayer.pause();
+    })
+    TrackPlayer.addEventListener(Event.RemotePlay,()=>{
+        TrackPlayer.play();
+    })
+    TrackPlayer.addEventListener(Event.RemoteNext,()=>{
+        TrackPlayer.skipToNext();
+    })
+    TrackPlayer.addEventListener(Event.RemotePrevious,()=>{
+        TrackPlayer.skipToPrevious();
+    })
 }
